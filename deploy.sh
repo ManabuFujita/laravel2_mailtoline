494094cf18fb065e4e5505b644984d943791856e
Fujita <manabu.fjt@gmail.com>
2026-06-07 22:15:03 +0900

    deploy.shの修正

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 git pull git@github.com:ManabuFujita/laravel2_mailtoline.git
+composer install --no-dev
 /usr/local/bin/php8.2 artisan config:cache
 /usr/local/bin/php8.2 artisan route:cache
 /usr/local/bin/php8.2 artisan view:cache
